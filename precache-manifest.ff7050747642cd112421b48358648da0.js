d4c88778d0ef5701d0bede0f67b8b257a9f3f66e
pipeza379 <kittapon.ku.ac.th>
2019-05-25 03:40:59 +0700

    Updates

---
+++ precache-manifest.ff7050747642cd112421b48358648da0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73d88847dfd9ab89483f89ed7265f03",
+    "url": "/KUSchedule/index.html"
+  },
+  {
+    "revision": "92d8f78d0f5a65b364be",
+    "url": "/KUSchedule/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "763353684ac2ad5ae8b3",
+    "url": "/KUSchedule/static/css/main.8b46eff5.chunk.css"
+  },
+  {
+    "revision": "92d8f78d0f5a65b364be",
+    "url": "/KUSchedule/static/js/2.2b51da73.chunk.js"
+  },
+  {
+    "revision": "763353684ac2ad5ae8b3",
+    "url": "/KUSchedule/static/js/main.aceb6f36.chunk.js"
+  },
+  {
+    "revision": "5dfff5d554c5f1c8460c",
+    "url": "/KUSchedule/static/js/runtime~main.bb8bd202.js"
+  },
+  {
+    "revision": "ca3efbf7e05bf999cf240b6cc2b75305",
+    "url": "/KUSchedule/static/media/add.ca3efbf7.svg"
+  },
+  {
+    "revision": "c3d75740c5c58e55c4e6e9bac7e742ac",
+    "url": "/KUSchedule/static/media/down.c3d75740.svg"
+  },
+  {
+    "revision": "b3996ce6639b269bf7d5944d644bfc1d",
+    "url": "/KUSchedule/static/media/edit-color.b3996ce6.svg"
+  },
+  {
+    "revision": "45b30f6af163a69443f70b9e363a9a6e",
+    "url": "/KUSchedule/static/media/edit.45b30f6a.svg"
+  },
+  {
+    "revision": "24a93bb6a956c41e0f5fde63df92e183",
+    "url": "/KUSchedule/static/media/up.24a93bb6.svg"
+  }
+]);
