5000176896509dca33d1040517480b2b4ebeeb0d
pipeza379 <kittapon.ku.ac.th>
2019-07-06 01:33:28 +0700

    -

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger'
-import reducer from './reducers';
+// import logger from 'redux-logger'
+// import reducer from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 const store = createStore(reducer)
